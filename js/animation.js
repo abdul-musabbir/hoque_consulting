a7f12c46fa7784c80563191103e607d4eb88c9fe
Abdul Musabbir <musabbir.net@gmail.com>
2024-12-27 00:32:24 +0600

    update branch name

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,23 @@
 "use strict";
 const sectionssss = document.querySelectorAll(".sectionssss");
 console.log(sectionssss);
-const observer = new IntersectionObserver((item) => {
+const observer = new IntersectionObserver(
+  (item) => {
     item.forEach((i) => {
-        if (i.isIntersecting) {
-            i.target.classList.add("show");
-            // i.target.classList.remove("show");
-        }
-        else {
-            i.target.classList.remove("show");
-        }
+      if (i.isIntersecting) {
+        i.target.classList.add("show");
+        // i.target.classList.remove("show");
+      } else {
+        i.target.classList.remove("show");
+      }
     });
-}, {
+  },
+  {
     threshold: 0.5,
-});
+  }
+);
 sectionssss.forEach((section) => {
-    observer.observe(section);
+  observer.observe(section);
 });
+
+// update branch name
